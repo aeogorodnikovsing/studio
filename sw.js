76562a22efa11487406c88032f8ed8848970d340
aeogorodnikovsing <aeogorodnikovsing@gmail.com>
2026-08-23 18:21:55 +0300

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Офлайн-кэш студии: cache-first, версия меняется при каждом обновлении файлов */
 'use strict';
 
-const CACHE = 'studio-v3';
+const CACHE = 'studio-v4';
 const ASSETS = [
   './',
   'index.html',
