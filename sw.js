1d0ee3740a01cf3a5528496652a199b72e862d8d
aeogorodnikovsing <aeogorodnikovsing@gmail.com>
2026-08-23 11:47:53 +0300

    deploy: sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+/* Офлайн-кэш студии: cache-first, версия меняется при каждом обновлении файлов */
+'use strict';
+
+const CACHE = 'studio-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'style.css',
+  'app.js',
+  'golos.ttf',
+  'marck.ttf',
+  'manifest.webmanifest',
+  'icon-192.png',
+  'icon-512.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then((hit) => {
+      if (hit) return hit;
+      return fetch(e.request).then((resp) => {
+        if (resp.ok && new URL(e.request.url).origin === location.origin) {
+          const copy = resp.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return resp;
+      });
+    })
+  );
+});
